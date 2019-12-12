1a8e3a6b5a4f8342c158ee9d672ea417495b9093
Will <willjthorpe@gmail.com>
2019-12-12 13:50:09 +0000

    add ingredients and method to recipe view screen

--- lib/pages/detail_recipe.dart
+++ lib/pages/detail_recipe.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/create_ingredient.dart';
-import '../http/fetch.dart';
-import 'create_ingredient.dart';
 
 class RecipeDetailPage extends StatefulWidget {
   RecipeDetailPage({Key key, this.title, this.data}) : super(key: key);
@@ -18,29 +15,98 @@ class _RecipeDetailPageState extends State<RecipeDetailPage> {
 
   @override
   Widget build(BuildContext context) {
-    final Map arguments = ModalRoute.of(context).settings.arguments as Map;
-    return Scaffold(
-        key: _scaffoldBookKey,
-        appBar: AppBar(
-          title: Text(arguments['title']),
-        ),
-        body: ListView(shrinkWrap: true, children: <Widget>[
-          new ListTile(
-            leading: const Icon(Icons.group),
-            title: Text(arguments['data']['tag']),
-          ),
-          new ListTile(
-            leading: const Icon(Icons.room_service),
-            title: Text(arguments['data']['servings'] + ' people'),
-          ),
-          new ListTile(
-            leading: const Icon(Icons.av_timer),
-            title: Text('Prep Time: ' + arguments['data']['prepTime'] + ' minutes'),
-          ),
-          new ListTile(
-            leading: const Icon(Icons.av_timer),
-            title: Text('Cooking Time: ' + arguments['data']['cookTime'] + ' minutes'),
-          ),
-        ]));
+    final Map arguments = ModalRoute
+        .of(context)
+        .settings
+        .arguments as Map;
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+            appBar: AppBar(
+                title: Text(arguments['title']),
+                bottom: TabBar(
+                    tabs: [
+                      Tab(icon: Icon(Icons.note_add)),
+                      Tab(icon: Icon(Icons.fastfood)),
+                      Tab(icon: Icon(Icons.subject)),
+                    ]
+                )
+            ),
+          body: TabBarView(
+            children: [
+              ListView(shrinkWrap: true, children: <Widget>[
+                  new ListTile(
+                    leading: const Icon(Icons.group),
+                    title: Text(arguments['data']['tag']),
+                  ),
+                  new ListTile(
+                    leading: const Icon(Icons.room_service),
+                    title: Text(arguments['data']['servings'] + ' people'),
+                  ),
+                  new ListTile(
+                    leading: const Icon(Icons.av_timer),
+                    title: Text('Prep Time: ' +
+                        arguments['data']['prepTime'] +
+                        ' minutes'),
+                  ),
+                  new ListTile(
+                    leading: const Icon(Icons.av_timer),
+                    title: Text('Cooking Time: ' +
+                        arguments['data']['cookTime'] +
+                        ' minutes'),
+                  ),
+                ]),
+            Container(
+                height: MediaQuery.of(context).size.height * 0.8,
+                child: ListView.separated(
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      if (arguments['data']['ingredients'][index]['type'] ==
+                          'number') {
+                        return ListTile(
+                          title: Text(
+                            '${arguments['data']['ingredients'][index]['amount']} ${arguments['data']['ingredients'][index]['name']}',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        );
+                      } else {
+                        return ListTile(
+                          title: Text(
+                            '${arguments['data']['ingredients'][index]['amount']} ${arguments['data']['ingredients'][index]['type']} of ${arguments['data']['ingredients'][index]['name']}',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                    separatorBuilder: (context, index) {
+                      return Divider();
+                    },
+                    itemCount: arguments['data']['ingredients'].length)),
+            Container(
+                height: MediaQuery.of(context).size.height * 0.8,
+                child: ListView.separated(
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(
+                          '${arguments['data']['method'][index]}',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return Divider();
+                    },
+                    itemCount: arguments['data']['method'].length)),
+            ]
+          )
+        )
+    );
   }
 }
