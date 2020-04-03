a9a736ef1c470e19537f9c27026f6ed646f7c37d
Will <willjthorpe@gmail.com>
2020-04-03 15:10:45 +0100

    add bash to yml

--- lib/codecov.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-main() {
-  // List all files in the current directory in UNIX-like systems.
-  Process.run('bash <(curl -s https://codecov.io/bash)',[]).then((ProcessResult results) {
-    print(results.stdout);
-  });
-}
